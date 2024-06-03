87cf525d7a5cfe25d4e87b1479a18d937a1d7fcc
Howf1706 <121178605+Howf1706@users.noreply.github.com>
2024-06-04 02:35:27 +0700

    fix sfx token
    check new repo

--- Billiard/Assets/Scripts/PauseMenu.cs
+++ Billiard/Assets/Scripts/PauseMenu.cs
@@ -2,9 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
+    [SerializeField] Slider _MusicSlider, _SFXSlider;
+    private void Start() {
+        if (PlayerPrefs.HasKey("MusicVolume") 
+        && PlayerPrefs.HasKey("SFXVolume")
+        ) {
+            LoadVolume();
+        }
+        else {
+            PlayerPrefs.SetFloat("MusicVolume", 0.5f);
+            PlayerPrefs.SetFloat("SFXVolume", 0.5f);
+            LoadVolume();
+        }
+    }
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
     void Update()
@@ -49,5 +63,23 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1f;
         GameIsPaused = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void MusicVolume()
+    {
+        AudioManager.instance.MusicVolume(_MusicSlider.value);
+        PlayerPrefs.SetFloat("MusicVolume", _MusicSlider.value);
+        StaticToken.musicVolume = _MusicSlider.value;
+    }
+    public void SFXVolume()
+    {
+        AudioManager.instance.SFXVolume(_SFXSlider.value);
+        PlayerPrefs.SetFloat("SFXVolume", _SFXSlider.value);
+        StaticToken.sfxVolume = _SFXSlider.value;
+    }
+    private void LoadVolume() {
+        _MusicSlider.value = StaticToken.musicVolume;
+        _SFXSlider.value = StaticToken.sfxVolume;
+        MusicVolume();
+        SFXVolume();
     }
 }
